2c424e0dcfbf7b6ed82880f06479070e3451c60e
unknown <liuxfeng2015@163.com>
2020-08-10 10:06:29 +0800

    初始化

---
+++ cc.ts
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello," + person
+}
+let user = "jane User";
+document.body.innerHTML = greeter(user)
---
+++ ceshi2.js
@@ -0,0 +1,7 @@
+(function () {
+    function greeter(person) {
+        return "Hello," + person;
+    }
+    var user = [1, 2, 3];
+    document.body.innerHTML = greeter(user);
+});
